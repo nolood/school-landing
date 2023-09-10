96afeb3c41ae34e2cd035c4e6267648b3d96f4f7
nolood <a9514548799@gmail.com>
2023-09-09 16:40:41 -0800

    feature: basic strucrure, start fsd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/shared/globals/styles/globals.css";
+import type { AppProps } from "next/app";
+import MainLayout from "@/app/layout/main-layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
 }
---
+++ src/widgets/header/ui/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="section">
+      <div className="container">Header</div>
+    </section>
+  );
+};
+
+export default Header;
